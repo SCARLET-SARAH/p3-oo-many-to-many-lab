f6de9d9ea2b18a5ae5eaa425b65a72323bfb49fe
scarlet <sarahscarlet641@gmail.com>
2024-06-09 19:20:46 +0300

    complete

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,56 @@
 class Author:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.contracts = []
+
+    def sign_contract(self, book, date, royalties):
+        contract = Contract(self, book, date, royalties)
+        self.contracts.append(contract)
+        return contract
+
+    def contracts(self):
+        return self.contracts
+
+    def books(self):
+        return [contract.book for contract in self.contracts]
+
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts)
 
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+        self.contracts = []
+
+    def contracts(self):
+        return self.contracts
+
+    def authors(self):
+        return [contract.author for contract in self.contracts]
 
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise TypeError("author must be an instance of Author")
+        if not isinstance(book, Book):
+            raise TypeError("book must be an instance of Book")
+        if not isinstance(date, str):
+            raise TypeError("date must be a string")
+        if not isinstance(royalties, int):
+            raise TypeError("royalties must be an integer")
+
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all.append(self)
+        author.contracts.append(self)
+        book.contracts.append(self)
+
+@classmethod
+def contracts_by_date(cls, date):
+        return [contract for contract in cls.all if contract.date == date]
